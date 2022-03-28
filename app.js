39dd586e21a72231ccdf831ac4617cad0d7a6e93
camillaUnicap <101574737+camillaUnicap@users.noreply.github.com>
2022-03-28 16:34:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const numeros = [];
+
+function vetor() {
+    const number = document.getElementById("numero").value;
+    console.log(number);
+    
+
+    numeros.push(number);
+    console.log(numeros);
+}
+
+function vetor1() {
+    const numbers = document.getElementById("numero").value;
+    console.log(numbers);
+    
+    numeros.push(numbers);
+    console.log(numeros);
+}
